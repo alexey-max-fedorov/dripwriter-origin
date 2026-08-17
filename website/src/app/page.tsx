1a7775ecc8813e6c9bab2172e3e52b7ec929a9a9
Alexey Fedorov <alexey.max.fedorov@gmail.com>
2026-08-17 16:53:04 -0700

    Update page.tsx

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -3,6 +3,7 @@ import { Footer } from "@/components/layout/Footer";
 import { Hero } from "@/components/sections/Hero";
 import { FeatureGrid } from "@/components/sections/FeatureGrid";
 import { InstallSteps } from "@/components/sections/InstallSteps";
+import { BrandClarification } from "@/components/sections/BrandClarification";
 import { FAQ } from "@/components/sections/FAQ";
 import { CTASection } from "@/components/sections/CTASection";
 
@@ -14,6 +15,7 @@ export default function HomePage() {
         <Hero />
         <FeatureGrid />
         <InstallSteps />
+        <BrandClarification />
         <FAQ />
         <CTASection />
       </main>
